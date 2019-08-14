de588f5c6b2a1eaa2b8450581a2ed34d0c7a8f57
Joel Robles <joel-robles-gasser@hotmail.ch>
2019-08-14 02:53:04 +0200

    Add files via upload

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,14 +10,18 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={()=>console.log("You clicked me")}>
-                        <Link to="/details">
-                            <img src={img} alt="product" className="card-img-top"/>
-                        </Link>
-                        <button className="cart-btn" disabled={inCart?true:false} onClick={()=>{console.log("added to the cart");}}>
-                            {inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>):(<i className="fas fa-cart-plus"></i>)}
-                        </button>                        
-                    </div>
+                    <ProductConsumer>
+                        {(value) => (
+                        <div className="img-container p-5" onClick={() => value.handleDetail(id)}>
+                            <Link to="/details">
+                                <img src={img} alt="product" className="card-img-top"/>
+                            </Link>
+                            <button className="cart-btn" disabled={inCart?true:false} onClick={()=> {value.addToCart(id); value.openModal(id);}}>
+                                {inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>):(<i className="fas fa-cart-plus"></i>)}
+                            </button>                        
+                        </div>
+                        )}                        
+                    </ProductConsumer>
                     {/* card footer */}
                     <div className="card-footer d-flex justify-content-between">
                         <p className="align-self-center mb-0">
@@ -56,6 +60,11 @@ const ProductWrapper = styled.div`
     transition: all 1s linear;
 }
 
+.cart-btn:hover{
+    color:var(--mainBlue);
+    cursor: pointer;
+}
+
 &:hover{
     .card{
         border:0.04rem solid rgba(0,0,0,0.2);
@@ -72,12 +81,10 @@ const ProductWrapper = styled.div`
     overflow:hidden;
 }
 
-.card-img-top{
-    transition: all 1s linear;
-}
 
 .img-container:hover .card-img-top{
     transform:scale(1.2);
+    transition: all 1s linear;
 }
 
 .cart-btn{
